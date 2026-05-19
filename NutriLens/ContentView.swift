de9ce78344809b0afe669c06d5e092f58a987d4f
Pooja Hosamane <poojahr91@gmail.com>
2026-05-19 19:58:54 -0400

    Initial Commit

---
+++ NutriLens/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NutriLens
+//
+//  Created by Pooja Hosamane on 2026-05-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NutriLens/NutriLensApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutriLensApp.swift
+//  NutriLens
+//
+//  Created by Pooja Hosamane on 2026-05-19.
+//
+
+import SwiftUI
+
+@main
+struct NutriLensApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
